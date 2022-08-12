bb8d1b207b217f6e5d3242bb6da828426559b9c6
teddybear27 <tidioukane@gmail.com>
2022-08-12 12:15:53 +0200

    add login config002

--- checkLogin.php
+++ checkLogin.php
@@ -9,9 +9,9 @@ if(!empty($_POST["email"]) && !empty($_POST["pwd"])) {
 
       $connect = connectDB();
       //Prepare la requête pour éviter les injections SQL
-   		$queryPrepared = $connect->prepare("SELECT status, mdp, blocked FROM user WHERE mail = :mail AND check_mail = 1");
+   		$queryPrepared = $connect->prepare("SELECT status, mdp, blocked FROM user WHERE mail = :mail AND check_mail = '1'");
 
-   		$queryPrepared->execute(["mail"=>$_POST["email"]]);
+   		$queryPrepared->execute(["mail"=>$email]);
         $result = $queryPrepared->fetch();
         if (empty($result)) {
           echo "Identifiants incorrects";
@@ -20,15 +20,15 @@ if(!empty($_POST["email"]) && !empty($_POST["pwd"])) {
         //récupère des valeurs de bdd et les met dans les variables $_SESSION
         else if (password_verify($_POST["pwd"], $result["pwd"])) {
           // vérifie si l'utilisateur est bloqué
-          if ($result["blocked"] == 'oui') {
+          /*if ($result["blocked"] == 'oui') {
             redirect("profile/blocked.php");
-          }
+          }*/
             $_SESSION["online"] = 'true';
             $_SESSION["user"] = $result["id"];          
 
-            if ($result["status"]  == "admin") {
+            /*if ($result["status"]  == "admin") {
               redirect("admin/admin.php");
-            }
+            }*/
 
   				//$_SESSION["token"] = createToken($_POST["email"]);
 
