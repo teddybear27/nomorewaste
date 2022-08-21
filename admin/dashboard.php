fa831bb1c0691bbe73847d2ee0443eb3fd30b7b6
teddybear27 <tidioukane@gmail.com>
2022-08-22 00:42:48 +0200

    coder liste particuliers03

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -211,22 +211,22 @@ require "../functions.php";
                   document.write(new Date().getFullYear())
                 </script>,
                 made with <i class="fa fa-heart"></i> by
-                <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Cheikh KANE.</a>
+                <a href="" class="font-weight-bold">Cheikh KANE.</a>
               </div>
             </div>
             <div class="col-lg-6">
               <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                 <li class="nav-item">
-                  <a href="" class="nav-link text-muted" target="_blank">Cheikh KANE</a>
+                  <a href="" class="nav-link text-muted">Cheikh KANE</a>
                 </li>
                 <li class="nav-item">
-                  <a href="" class="nav-link text-muted" target="_blank">About Us</a>
+                  <a href="" class="nav-link text-muted">About Us</a>
                 </li>
                 <li class="nav-item">
-                  <a href="" class="nav-link text-muted" target="_blank">Blog</a>
+                  <a href="" class="nav-link text-muted">Blog</a>
                 </li>
                 <li class="nav-item">
-                  <a href="" class="nav-link pe-0 text-muted" target="_blank">License</a>
+                  <a href="" class="nav-link pe-0 text-muted">License</a>
                 </li>
               </ul>
             </div>
--- admin/particuliers.php
+++ admin/particuliers.php
@@ -242,16 +242,16 @@ if (isset($res)){
             <div class="col-lg-6">
               <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                 <li class="nav-item">
-                  <a href="" class="nav-link text-muted" target="_blank">Cheikh KANE</a>
+                  <a href="" class="nav-link text-muted">Cheikh KANE</a>
                 </li>
                 <li class="nav-item">
-                  <a href="" class="nav-link text-muted" target="_blank">About Us</a>
+                  <a href="" class="nav-link text-muted">About Us</a>
                 </li>
                 <li class="nav-item">
-                  <a href="" class="nav-link text-muted" target="_blank">Blog</a>
+                  <a href="" class="nav-link text-muted">Blog</a>
                 </li>
                 <li class="nav-item">
-                  <a href="" class="nav-link pe-0 text-muted" target="_blank">License</a>
+                  <a href="" class="nav-link pe-0 text-muted">License</a>
                 </li>
               </ul>
             </div>
