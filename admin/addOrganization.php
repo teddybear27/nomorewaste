645faa50598638819b94bba77ddf616086550a68
teddybear27 <tidioukane@gmail.com>
2022-08-25 00:47:09 +0200

    ajouter utlisateur by admin01

--- admin/addOrganization.php
+++ admin/addOrganization.php
@@ -124,9 +124,10 @@ if ($_SESSION['sid'] != 1){
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Admin</a></li>
-            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Particuliers</li>
+            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Associations</li>
+            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Ajout association</li>
           </ol>
-          <h6 class="font-weight-bolder mb-0">Particuliers</h6>
+          <h6 class="font-weight-bolder mb-0">Ajout association</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
--- admin/addUser.php
+++ admin/addUser.php
@@ -125,8 +125,9 @@ if ($_SESSION['sid'] != 1){
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Admin</a></li>
             <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Particuliers</li>
+            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Ajout utilisateur</li>
           </ol>
-          <h6 class="font-weight-bolder mb-0">Particuliers</h6>
+          <h6 class="font-weight-bolder mb-0">Ajout utilisateur</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
