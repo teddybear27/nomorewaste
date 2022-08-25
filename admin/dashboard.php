9b65de65319dd280bcc811f580d7b4e2e2598640
teddybear27 <tidioukane@gmail.com>
2022-08-25 11:52:26 +0200

    icones dans le dashboard04

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -96,7 +96,7 @@ $resCartsToShip = getCartsToShip($connect);
         <li class="nav-item">
           <a class="nav-link text-white " href="../pages/notifications.html">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="fa-brands fa-github-square"></i>
+              <i class="material-icons opacity-10">notifications</i>
             </div>
             <span class="nav-link-text ms-1">Notifications</span>
           </a>
