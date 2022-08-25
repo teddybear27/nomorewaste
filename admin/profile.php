b649409a66370ca6812c42e926ed9088fc49e2db
teddybear27 <tidioukane@gmail.com>
2022-08-25 15:02:52 +0200

    test affichage02

--- admin/profile.php
+++ admin/profile.php
@@ -161,8 +161,8 @@ $data = $resCurrent->fetch();
                       <h6 class="mb-0">Informations Personnelles</h6>
                     </div>
                     <div class="col-md-4 text-end">
-                      <a href="javascript:;">
-                        <i class="fas fa-user-edit text-secondary text-sm" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit Profile"></i>
+                      <a href="modifyProfile.php">
+                        <i class="fas fa-user-edit text-secondary text-sm" data-bs-toggle="tooltip" data-bs-placement="top" title="Editer"></i>
                       </a>
                     </div>
                   </div>
