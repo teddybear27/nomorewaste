643a975976fd6028750c807cf6826aca16529891
teddybear27 <tidioukane@gmail.com>
2022-08-25 14:31:44 +0200

    supprimer quelques liens

--- admin/profile.php
+++ admin/profile.php
@@ -19,7 +19,7 @@ $resCurrent = getCurrentUser($connect,$_SESSION['mail']);
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="../assets/img/favicon.png">
   <title>
-    Dashboard Administrateur
+    Infos Personelles
   </title>
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
@@ -38,7 +38,7 @@ $resCurrent = getCurrentUser($connect,$_SESSION['mail']);
   <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard " target="_blank">
+      <a class="navbar-brand m-0" href="" target="_blank">
         <img src="../assets/img/logo.png" class="navbar-brand-img h-100" alt="main_logo">
         <span class="ms-1 font-weight-bold text-white">No More Waste</span>
       </a>
