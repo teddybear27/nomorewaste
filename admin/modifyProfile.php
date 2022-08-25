75c31f43b37a904157e5afecabf1e39df25bfbbf
teddybear27 <tidioukane@gmail.com>
2022-08-25 17:10:18 +0200

    test modifier profil03

--- admin/modifyProfile.php
+++ admin/modifyProfile.php
@@ -180,7 +180,17 @@ $data = $resCurrent->fetch();
             </div>
   <div class="col-12 col-xl-4">
     <div class="card card-plain h-100">
-      <div class="card-body p-3">                  
+      <div class="card-body p-3">
+        <div class="row">
+          <div class="col-md-8 d-flex align-items-center">
+            <h6 class="mb-0">Modification Informations</h6>
+          </div>
+          <div class="col-md-4 text-end">
+            <a href="modifyProfile.php">
+              <i class="fas fa-user-edit text-secondary text-sm" data-bs-toggle="tooltip" data-bs-placement="top" title="Valider"></i>
+            </a>
+          </div>
+        </div>                  
         <hr class="horizontal gray-light my-4">
         <ul class="list-group">
           <li class="list-group-item border-0 ps-0 pt-0 text-sm"><strong class="text-dark">Nom:</strong> &nbsp;
@@ -197,6 +207,12 @@ $data = $resCurrent->fetch();
           </li>
           <li class="list-group-item border-0 ps-0 text-sm"><strong class="text-dark">Email:</strong> &nbsp; 
             <input type="text" name="email" placeholder="example@email.com" value="<?=$data["mail"]?>" required>
+          </li>
+          <li class="list-group-item border-0 ps-0 text-sm"><strong class="text-dark">Mot de Passe:</strong> &nbsp; 
+            <input type="password" name="pwd" placeholder="Mot de passe" value="<?=$data["mdp"]?>" required>
+          </li>
+          <li class="list-group-item border-0 ps-0 text-sm"><strong class="text-dark">Verification Mot de Passe:</strong> &nbsp; 
+            <input type="password" name="pwdConfirm" placeholder="Verification mot de passe" required>
           </li>
           <li class="list-group-item border-0 ps-0 text-sm"><strong class="text-dark">Adresse:</strong> &nbsp; 
             <input type="text" name="address" placeholder="123 rue emile" value="<?=$data["adresse"]?>" required>
