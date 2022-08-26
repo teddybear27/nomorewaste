51e6b4d80c27da547f80679c48fb805b4e3216f0
teddybear27 <tidioukane@gmail.com>
2022-08-26 13:04:23 +0200

    ajout partie modif assos

--- admin/modifyOrganizationBack.php
+++ admin/modifyOrganizationBack.php
@@ -2,12 +2,11 @@
 session_start();
 require "../functions.php";
 
-if( count($_POST) == 11 
-	&& !empty($_POST["shopname"])
-	&& !empty($_POST["category"])
+if( count($_POST) == 10 
+	&& !empty($_POST["organizationName"])
 	&& !empty($_POST["siren"])
 	&& !empty($_POST["email"])
-	&& !empty($_POST["regYear"])
+	&& !empty($_POST["creationYear"])
 	&& !empty($_POST["phone"])
 	&& !empty($_POST["address"])
 	&& !empty($_POST["zip"])
@@ -17,8 +16,7 @@ if( count($_POST) == 11
 	$connect = connectDB();
 
 	//Nettoyage
-	$_POST["shopname"] = ucwords(strtolower(trim($_POST["shopname"])));
-	$_POST["category"] = ucwords(strtolower(trim($_POST["category"])));
+	$_POST["organizationName"] = ucwords(strtolower(trim($_POST["organizationName"])));
 	$_POST["siren"] = trim($_POST["siren"]);
 	$_POST["phone"] = trim($_POST["phone"]);
 	$_POST["email"] = strtolower(trim($_POST["email"]));
@@ -29,30 +27,24 @@ if( count($_POST) == 11
 
 	$error = false;
 	$listOfErrors = [];
-	$shopMail = $_POST['modifyS'];
+	$organizationMail = $_POST['modifyO'];
 
-	//shopname
+	//organizationName
 
-	if( strlen($_POST["shopname"])<2  || strlen($_POST["shopname"])>100 ){
+	if( strlen($_POST["organizationName"])<2  || strlen($_POST["organizationName"])>100 ){
 			$error = true;
 			$listOfErrors[] = "Votre nom doit faire entre 2 et 100 caractères";
 	}
 
-	//category
-
-	if( strlen($_POST["category"])<2  || strlen($_POST["category"])>100 ){
-			$error = true;
-			$listOfErrors[] = "Votre prénom doit faire entre 2 et 50 caractères";
-	}
-
 	//SIREN
 
-	if( strlen($_POST["siren"])<9  || strlen($_POST["siren"])>9 ){
+	if( strlen($_POST["siren"]) != 9){
 			$error = true;
 			$listOfErrors[] = "Votre numero de SIREN doit faire 9 caractères";
 	}
+
 $mailChanged = 0;
-if ($shopMail != $_POST["email"]){
+if ($organizationMail != $_POST["email"]){
 	// Vérifier email
 
 	if( !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) ){
@@ -60,16 +52,16 @@ if ($shopMail != $_POST["email"]){
 			$listOfErrors[] = "Votre email est incorrect";
 	}else if(  emailExist($connect, $_POST['email'])  ){
 			$error = true;
-			$listOfErrors[] = "Cette adresse mail est déjà utilisé";
+			$listOfErrors[] = "Cette adresse mail est déjà utilisée";
 	}
 	$mailChanged = 1;
 }
 
-	// regYear
+	// creationYear
 
-	if( strlen($_POST["regYear"])<4  || strlen($_POST["regYear"])>4 ){
+	if( strlen($_POST["creationYear"]) != 4){
 			$error = true;
-			$listOfErrors[] = "Votre Année Immatriculation doit faire 4 caractères";
+			$listOfErrors[] = "L'Année de création doit faire 4 caractères";
 	}
 
 
@@ -84,22 +76,21 @@ if ($shopMail != $_POST["email"]){
 	if($error){
 		
 			setcookie("errorForm", serialize($listOfErrors));	
-			header("Location: modifyShop.php");
+			header("Location: modifyOrganization.php");
 
 	}else{		
 		if ($mailChanged == 1){
-			$queryPrepared = $connect->prepare("UPDATE shop SET nom = :nom, categorie = :categorie, siren = :siren, mail = :mail, annee_immatriculation = :annee_immatriculation, numero_telephone = :numero_telephone, adresse = :adresse, code_postal = :code_postal, ville = :ville, pays = :pays, check_mail = :check_mail WHERE mail = '$shopMail'");
+			$queryPrepared = $connect->prepare("UPDATE organization SET nom = :nom, siren = :siren, mail = :mail, annee_creation = :annee_creation, numero_telephone = :numero_telephone, adresse = :adresse, code_postal = :code_postal, ville = :ville, pays = :pays, check_mail = :check_mail WHERE mail = '$organizationMail'");
 
-			$shopname = htmlspecialchars($_POST["shopname"]);
-			$verifKey = md5(time().$shopname); //Génère une clé avec le nom
+			$organizationName = htmlspecialchars($_POST["organizationName"]);
+			$verifKey = md5(time().$organizationName); //Génère une clé avec le nom
 
 			$queryPrepared->execute(
 				[
-					"nom"=>$_POST["shopname"],
-					"categorie"=>$_POST["category"],
+					"nom"=>$_POST["organizationName"],
 					"siren" =>$_POST["siren"],
 					"mail"=>$_POST["email"],
-					"annee_immatriculation" => $_POST["regYear"],
+					"annee_creation" => $_POST["creationYear"],
 					"numero_telephone" => $_POST["phone"],
 					"adresse" => $_POST["address"],
 					"code_postal" => $_POST["zip"],
@@ -118,8 +109,8 @@ if ($shopMail != $_POST["email"]){
 		    if($queryPrepared) {
 		        $to = $email;
 		        $subject = "NoMoreWaste : Vérification du mail";
-		        $message = " Veuillez cliquer sur le lien suivant afin de vérifier votre compte : <a href='https://nomorewaste.online/mail/verifyMailShop.php?code_verif=$verifKey'>Valider mon compte</a><br/>\n ";
-		        $message .= "En cas de problème essayez ce lien : https://nomorewaste.online/mail/verifyMailShop.php?code_verif=$verifKey";
+		        $message = " Veuillez cliquer sur le lien suivant afin de vérifier votre compte : <a href='https://nomorewaste.online/mail/verifyMailOrganization.php?code_verif=$verifKey'>Valider mon compte</a><br/>\n ";
+		        $message .= "En cas de problème essayez ce lien : https://nomorewaste.online/mail/verifyMailOrganization.php?code_verif=$verifKey";
 		        $header="MIME-Version: 1.0\r\n";
 		        $header.='Content-Type:text/html; charset="uft-8"'."\r\n";
 		        $header.='Content-Transfer-Encoding: 8bit'."\r\n";
@@ -128,14 +119,13 @@ if ($shopMail != $_POST["email"]){
 		    }
     		$listOfErrors[] = ["Un mail de confirmation vous a été envoyé (Voir spams / courriers indésirables)"];
 		}else{
-			$queryPrepared = $connect->prepare("UPDATE shop SET nom = :nom, categorie = :categorie, siren = :siren, annee_immatriculation = :annee_immatriculation, numero_telephone = :numero_telephone, adresse = :adresse, code_postal = :code_postal, ville = :ville, pays = :pays WHERE mail = '$shopMail'");
+			$queryPrepared = $connect->prepare("UPDATE organization SET nom = :nom, siren = :siren, annee_creation = :annee_creation, numero_telephone = :numero_telephone, adresse = :adresse, code_postal = :code_postal, ville = :ville, pays = :pays WHERE mail = '$organizationMail'");
 
 			$queryPrepared->execute(
 				[
-					"nom"=>$_POST["shopname"],
-					"categorie"=>$_POST["category"],
+					"nom"=>$_POST["organizationName"],
 					"siren" =>$_POST["siren"],
-					"annee_immatriculation" => $_POST["regYear"],
+					"annee_creation" => $_POST["creationYear"],
 					"numero_telephone" => $_POST["phone"],
 					"adresse" => $_POST["address"],
 					"code_postal" => $_POST["zip"],
@@ -146,7 +136,7 @@ if ($shopMail != $_POST["email"]){
 			);
 		}
     setcookie("errorForm", serialize($listOfErrors));
-    redirect("commerces.php");
+    redirect("associations.php");
     die();
 		
 	}
